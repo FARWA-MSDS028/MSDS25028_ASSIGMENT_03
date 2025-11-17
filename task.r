107fef45a362e8204be83d6632eb95f8c02b6d0b
fARWA <msds25028@itu.edu.pk>
2025-11-17 20:42:02 +0500

    Inital commit: load dataset

---
+++ task.r
@@ -0,0 +1,21 @@
+#install.packages("data.table")
+#install.packages("ggplot2")
+library(data.table)
+library(ggplot2)
+film          <- fread("film.csv")
+language      <- fread("language.csv")
+actor         <- fread("actor.csv")
+film_actor    <- fread("film_actor.csv")
+category      <- fread("category.csv")
+film_category <- fread("film_category.csv")
+rental        <- fread("rental.csv")
+inventory     <- fread("inventory.csv")
+customer      <- fread("customer.csv")
+payment       <- fread("payment.csv")
+staff         <- fread("staff.csv")
+store         <- fread("store.csv")
+
+
+head(film)
+View(head(film))
+
